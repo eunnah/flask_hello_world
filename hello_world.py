fb2de7a5b6067d01e039ecdabf4594a7550e43a8
eunnah <eunnah@gmail.com>
2016-08-02 03:11:26 +0000

    Adding templates to hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,36 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 from os import environ
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/hello")
 def say_hi():
-    return "Hello World!"
-    
+    return render_template('template.html', my_string="Hello World!",
+                          current_time=datetime.datetime.now())
     
 @app.route("/hello/<name>")
 def hi_person(name):
-    return "Hello {}!".format(name.title())
-
-@app.route("/hello/<name>")
-def hello_person(name):
-    html = """
-        <h1>
-            Hello {}!
-        </h1>
-        <p>
-            Here's a picture of a kitten.  Awww...
-        </p>
-        <img src="http://placekitten.com/g/200/300">
-    """
-    return html.format(name.title())
+    return render_template('template2.html', my_string=name.title(),
+                          current_time=datetime.datetime.now())
     
 @app.route("/jedi/<firstname>/<lastname>")
 def jedi_name(firstname, lastname):
-    return "Your Jedi name is {}{}!".format(lastname[0:3].title(), firstname[0:2])
+    return render_template('template3.html', last=lastname[0:3].title(), first=firstname[0:2])
+    
+@app.template_filter()
+def datetimefilter(value, format='%Y/%m/%d %H:%M'):
+    """Convert a datetime to a different format."""
+    return value.strftime(format)
     
 if __name__ == "__main__":
     app.run(host=environ['IP'],
             port=int(environ['PORT']))
+
         
